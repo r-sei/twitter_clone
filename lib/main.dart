41ed4dc98609282b7de82fbecac59803306c4bbb
r-sei <ryousei3964@gmail.com>
2025-03-03 14:21:18 +0900

    📝 firebaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/firebase_options.dart';
 import 'package:twitter_clone/view/home_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
