c77be86ac3aeb33f3a9f49dba61708351889de46
r-sei <ryousei3964@gmail.com>
2025-03-04 22:50:12 +0900

    ⚡️ drawerのレイアウト修正

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -11,46 +11,52 @@ class SideMenu extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          const DrawerHeader(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              ProfileIcon(iconSize: 24),
-              Text(
-                'hoge',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                ),
-              ),
-              Text(
-                '@hoge',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    '${1}Followers',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
+          const Expanded(
+            child: DrawerHeader(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ProfileIcon(iconSize: 24),
+                    Gap(4),
+                    Text(
+                      'hoge',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                  Gap(8),
-                  Text(
-                    '${1}Following',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
+                    Gap(4),
+                    Text(
+                      '@hoge',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ]),
+                    Gap(4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${1}Followers',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Gap(8),
+                        Text(
+                          '${1}Following',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+            ),
           ),
           ListTile(
             leading: const Icon(Icons.account_circle),
